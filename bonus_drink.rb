10a13ea950aeb7ee0a4c9384bc5c8524b333538e
佐藤健人 <122395360+satotaketo@users.noreply.github.com>
2023-01-18 15:02:06 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -3,3 +3,15 @@ class BonusDrink
     0
   end
 end
+
+buy = 100
+drink = buy
+puts "買った数"
+puts buy
+while buy >= 3
+    exchange = ( buy / 3 ) + ( buy % 3 ) 
+    drink += ( buy / 3 )
+    buy = 0 + exchange
+end
+puts "飲んだ数" #答え
+puts drink
